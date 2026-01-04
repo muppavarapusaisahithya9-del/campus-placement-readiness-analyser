47d349cca8b098229aa8fe8ce8e01efc728f8917
muppavarapusaisahithya9-del <muppavarapusaisahithya9@gmail.com>
2026-01-04 10:29:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function login(){
+    window.location.href="dashboard.html";
+}
+function register(){
+    window.location.href="dashboard.html";
+}
+
+function goAssessment(){
+    window.location.href="assessment.html";
+}
+
+function goAnalysis(){
+    window.location.href="analysis.html";
+}
+
+function goProgress(){
+    window.location.href="progress.html";
+}
+
+function submitAssessment(){
+    window.location.href="analysis.html";
+}
+
+function goDashboard(){
+    window.location.href="dashboard.html";
+}
+
+
